ffa1bfde108c4d7cab2edee8a42b494cb6cdac35
soksof <sokratis.sofianopoulos@gmail.com>
2020-06-18 13:19:08 +0300

    Added A UserValidator class

---
+++ src/main/java/gr/example/boot/securityexample/validator/UserValidator.java
@@ -0,0 +1,44 @@
+package gr.example.boot.securityexample.validator;
+
+import gr.example.boot.securityexample.model.User;
+import gr.example.boot.securityexample.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    private final UserService userService;
+
+    public UserValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty");
+        if (user.getEmail().length() < 6 || user.getEmail().length() > 40) {
+            errors.rejectValue("email", "Size.userForm.email");
+        }
+        if (userService.findByEmail(user.getEmail()).isPresent()) {
+            errors.rejectValue("email", "Duplicate.userForm.email");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+        if (!user.getPasswordConfirm().equals(user.getPassword())) {
+            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+        }
+    }
+}
